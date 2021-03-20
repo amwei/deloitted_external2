4b9e187387fa3b268640803ee46f0d8945ec9fd1
amwei <amwei@deloitte.com>
2021-03-20 18:07:03 +0000

    add files

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Team Website - Deloitted to see you 2</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>Cool Events Only</h1>"));
         return done();
       });
 
